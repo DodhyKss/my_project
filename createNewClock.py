ba4ddc296276b9c2573daceb3a0bcb59d77e7299
DodhyKss <65852126+DodhyKss@users.noreply.github.com>
2022-01-13 17:27:15 +0800

    Update createNewClock.py

--- createNewClock.py
+++ createNewClock.py
@@ -1,5 +1,5 @@
-def new_date(jam_baru, menit_baru):
-	counter_x = int(menit_baru / 60)
+def new_date(new_clock, new_minute):
+	counter_x = int(new_minute / 60)
 	counter_z = 0
 	for i in range(counter_x):
 		if counter_z < 12:
@@ -7,32 +7,32 @@ def new_date(jam_baru, menit_baru):
 		else:
 			counter_z = 0
 			counter_z += 1
-	counter_y = menit_baru
+	counter_y = new_minute
 
-	if menit_baru >= 60:
-		menit_baru = int(menit_baru % 60)
-		if jam_baru == 12 and counter_y > 120 :
-			jam_baru = counter_z
-		elif jam_baru == 12 and counter_y < 120:
-			jam_baru = 1
+	if new_minute >= 60:
+		new_minute = int(new_minute % 60)
+		if new_clock == 12 and counter_y > 120 :
+			new_clock = counter_z
+		elif new_clock == 12 and counter_y < 120:
+			new_clock = 1
 		elif counter_x > 1:
-			jam_baru += counter_z
-			if jam_baru >= 12:
+			new_clock += counter_z
+			if new_clock >= 12:
 				for i in range(counter_z):
-					if jam_baru < 12:
-						jam_baru += 1
+					if new_clock < 12:
+						new_clock += 1
 					else:
-						jam_baru = 0		
+						new_clock = 0		
 		else:
-			jam_baru += 1 	 
+			new_clock += 1 	 
 
-	if jam_baru == 0 and menit_baru == 0:
+	if new_minute == 0 and new_minute == 0:
 		print('input salah') 
-	elif jam_baru >= 10 and menit_baru >= 10:
-		print("{0}:{1}".format(jam_baru,menit_baru))
-	elif jam_baru >= 10 and  menit_baru <= 10:
-		print("{0}:0{1}".format(jam_baru,menit_baru))
-	elif jam_baru <= 10 and menit_baru >= 10:
-		print("0{0}:{1}".format(jam_baru,menit_baru))
-	elif jam_baru <= 10 and menit_baru <= 10:
-		print('0{0}:0{1}'.format(jam_baru,menit_baru))
+	elif new_clock >= 10 and new_minute >= 10:
+		print("{0}:{1}".format(new_clock,new_minute))
+	elif new_clock >= 10 and  new_minute <= 10:
+		print("{0}:0{1}".format(new_clock,new_minute))
+	elif new_clock <= 10 and new_minute >= 10:
+		print("0{0}:{1}".format(new_clock,new_minute))
+	elif new_clock <= 10 and new_minute <= 10:
+		print('0{0}:0{1}'.format(new_clock,new_minute))
